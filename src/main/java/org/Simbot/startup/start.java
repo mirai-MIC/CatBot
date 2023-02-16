5a3abcb50256706a0eac4b056ba4644066a1605e
mirai-MIC <3092179918@qq.com>
2023-02-16 19:08:16 +0800

    Update Launch prompt messages

---
+++ src/main/java/org/Simbot/startup/start.java
@@ -0,0 +1,39 @@
+package org.Simbot.startup;
+
+import lombok.extern.slf4j.Slf4j;
+import org.Simbot.utils.FormatTime;
+import org.Simbot.utils.Properties.properties;
+import org.Simbot.utils.SendMsgUtil;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+/**
+ * @BelongsProject: simbot
+ * @BelongsPackage: org.Simbot.startup
+ * @Author: MIC
+ * @CreateTime: 2023-02-16  15:18
+ * @Description:
+ * @Version: 1.0
+ */
+
+@Component
+@Slf4j
+public class start implements ApplicationRunner {
+
+    @lombok.Getter
+    @Deprecated
+    String master = new properties().getProperties("cache/application.properties", "user.Master");
+
+    public start() throws IOException {
+    }
+
+    @Override
+    public void run(ApplicationArguments args) {
+        log.info("====================启动成功=====================");
+        SendMsgUtil.sendFriendMessage(Long.parseLong(getMaster()), "====MusicCatBot启动成功====\n时间: " + new FormatTime().getTime() + "\n功能预览: https://github.com/mirai-MIC/CatBot#readme");
+
+    }
+}

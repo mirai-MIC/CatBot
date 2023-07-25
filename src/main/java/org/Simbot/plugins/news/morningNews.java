1a8087c5587c660bcf9cf2919c34a6a090612706
ycvk <ycvk@icloud.com>
2023-07-25 12:08:53 +0800

    Update: 修改更好的早报获取源为v2.alapi.cn/api/zaobao

--- src/main/java/org/Simbot/plugins/news/morningNews.java
+++ src/main/java/org/Simbot/plugins/news/morningNews.java
@@ -1,12 +1,18 @@
 package org.Simbot.plugins.news;
 
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
 import lombok.extern.slf4j.Slf4j;
 import love.forte.simboot.annotation.Filter;
 import love.forte.simboot.annotation.Listener;
 import love.forte.simbot.event.GroupMessageEvent;
+import org.Simbot.utils.OK3HttpClient;
 import org.Simbot.utils.SendMsgUtil;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @BelongsProject: simbot
@@ -24,11 +30,21 @@ public class morningNews {
     @Value("${api.news}")
     String newsImage;
 
+    @Value("${news.token}")
+    String token;
+
     @Listener
     @Filter(value = "/每日早报")
     @Filter(value = "/早报")
     public void getNews(final GroupMessageEvent event) {
-        SendMsgUtil.sendSimpleGroupImage(event.getGroup(), event.getAuthor().getId(), "每日早报~", newsImage);
+        final Map<String, Object> map = new HashMap<>();
+        map.put("token", token);
+        OK3HttpClient.httpGetAsync(newsImage, map, null, result -> {
+            final JSONObject obj = JSONUtil.parseObj(result);
+            final String str = obj.getJSONObject("data").getStr("image");
+            log.info("result:{}", str);
+            SendMsgUtil.sendSimpleGroupImage(event.getGroup(), event.getAuthor().getId(), "每日早报~", str);
+        }, error -> log.error("error:{}", error.getMessage()));
     }
 
 }
